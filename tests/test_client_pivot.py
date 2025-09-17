8a2dba23bb409db789f2824ca34fe21a1374c3f8
hortemo <henrikhortemo@icloud.com>
2025-09-17 12:16:09 +0200

    test: cover pivot events dataframe

---
+++ tests/test_client_pivot.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+import pandas as pd
+
+from ga4bigquery.core.client import GA4BigQuery
+
+
+def test_pivot_events_dataframe_without_grouping_single_event() -> None:
+    df = pd.DataFrame(
+        {
+            "interval": [pd.Timestamp("2023-01-02"), pd.Timestamp("2023-01-01")],
+            "event_name": ["sign_up", "sign_up"],
+            "value": [5, 3],
+        }
+    )
+
+    result = GA4BigQuery._pivot_events_dataframe(df, "interval", [], ["sign_up"])
+
+    expected = pd.DataFrame(
+        {"value": [3, 5]},
+        index=pd.DatetimeIndex(
+            [pd.Timestamp("2023-01-01"), pd.Timestamp("2023-01-02")],
+            name="interval",
+        ),
+    )
+
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_pivot_events_dataframe_multiple_events() -> None:
+    df = pd.DataFrame(
+        {
+            "interval": [
+                pd.Timestamp("2023-01-01"),
+                pd.Timestamp("2023-01-01"),
+                pd.Timestamp("2023-01-02"),
+            ],
+            "event_name": ["purchase", "sign_up", "purchase"],
+            "value": [2, 4, 1],
+        }
+    )
+
+    result = GA4BigQuery._pivot_events_dataframe(
+        df,
+        "interval",
+        [],
+        ["sign_up", "purchase"],
+    )
+
+    expected = pd.DataFrame(
+        {
+            "purchase": [2.0, 1.0],
+            "sign_up": [4.0, 0.0],
+        },
+        index=pd.DatetimeIndex(
+            [pd.Timestamp("2023-01-01"), pd.Timestamp("2023-01-02")],
+            name="interval",
+        ),
+        columns=pd.Index(["purchase", "sign_up"], name="event_name"),
+    )
+
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_pivot_events_dataframe_with_custom_dimensions() -> None:
+    df = pd.DataFrame(
+        {
+            "interval": [
+                pd.Timestamp("2023-01-01"),
+                pd.Timestamp("2023-01-01"),
+                pd.Timestamp("2023-01-01"),
+                pd.Timestamp("2023-01-02"),
+                pd.Timestamp("2023-01-02"),
+            ],
+            "platform": ["iOS", "Android", "iOS", "iOS", "Android"],
+            "event_name": ["purchase", "purchase", "sign_up", "purchase", "sign_up"],
+            "value": [2, 3, 5, 4, 1],
+        }
+    )
+
+    result = GA4BigQuery._pivot_events_dataframe(
+        df,
+        "interval",
+        ["platform"],
+        ["sign_up", "purchase"],
+    )
+
+    expected = pd.DataFrame(
+        data=[[3.0, 0.0, 2.0, 5.0], [0.0, 1.0, 4.0, 0.0]],
+        index=pd.DatetimeIndex(
+            [pd.Timestamp("2023-01-01"), pd.Timestamp("2023-01-02")],
+            name="interval",
+        ),
+        columns=pd.MultiIndex.from_product(
+            [["Android", "iOS"], ["purchase", "sign_up"]],
+            names=["platform", "event_name"],
+        ),
+    )
+
+    pd.testing.assert_frame_equal(result, expected)
