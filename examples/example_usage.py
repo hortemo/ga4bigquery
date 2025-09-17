f3c464c140c4ef955034b34b41ec2defc0449298
hortemo <henrikhortemo@icloud.com>
2025-09-17 19:35:25 +0200

    Use public GA4 sample dataset in docs and example

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -1,39 +1,35 @@
 
-from datetime import date, timedelta
+from datetime import date
 from google.cloud import bigquery
 from ga4bigquery import GA4BigQuery, FunnelStep
 
-TABLE_ID = "photo-roulette-40b87.analytics_163566231.events_*"
+TABLE_ID = "bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*"
 TZ = "America/Los_Angeles"
-USER_ID_COL = "user_id"
+USER_ID_COL = "user_pseudo_id"
 
 client = bigquery.Client()
 ga = GA4BigQuery(table_id=TABLE_ID, tz=TZ, user_id_col=USER_ID_COL, client=client)
 
-active_android = ga.request_events(
-    events=["game_started"],
-    start=date.today() - timedelta(days=7),
-    end=date.today(),
-    measure="uniques",
-    filters=[{"prop": "platform", "op": "IN", "values": ["ANDROID"]}],
-    group_by="app_info.version",
-    interval="day",
-)
-print(active_android.tail())
-
-steps = [
-    FunnelStep(event_name="continue_from_welcome_screen"),
-    FunnelStep(
-        event_name="game_started",
-        conversion_window_gt=timedelta(minutes=0),
-        conversion_window_lt=timedelta(minutes=60),
-    ),
-]
-new_to_activated = ga.request_funnel(
-    steps=steps,
-    start=date.today() - timedelta(days=14),
-    end=date.today(),
+page_views = ga.request_events(
+    events=["page_view"],
+    start=date(2020, 11, 1),
+    end=date(2020, 11, 7),
+    measure="totals",
     group_by="platform",
     interval="day",
 )
-print(new_to_activated.tail())
+print(page_views.tail())
+
+steps = [
+    FunnelStep(event_name="view_item"),
+    FunnelStep(event_name="add_to_cart"),
+    FunnelStep(event_name="purchase"),
+]
+purchase_funnel = ga.request_funnel(
+    steps=steps,
+    start=date(2020, 11, 1),
+    end=date(2020, 11, 7),
+    group_by="platform",
+    interval="day",
+)
+print(purchase_funnel.tail())
