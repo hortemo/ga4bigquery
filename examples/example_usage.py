983818d98a3d34f9d454d68e25c2e58a0c89d9af
hortemo <henrikhortemo@icloud.com>
2025-09-18 16:01:40 +0200

    Initial commit

---
+++ examples/example_usage.py
@@ -0,0 +1,37 @@
+
+from datetime import date
+
+from google.cloud import bigquery
+
+from ga4bigquery import FunnelStep, GA4BigQuery
+
+TABLE_ID = "bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*"
+TZ = "America/Los_Angeles"
+USER_ID_COL = "user_pseudo_id"
+
+client = bigquery.Client()
+ga = GA4BigQuery(table_id=TABLE_ID, tz=TZ, user_id_col=USER_ID_COL, client=client)
+
+page_views = ga.request_events(
+    events=["page_view"],
+    start=date(2020, 11, 1),
+    end=date(2020, 11, 7),
+    measure="totals",
+    group_by="platform",
+    interval="day",
+)
+print(page_views.tail())
+
+steps = [
+    FunnelStep(event_name="view_item"),
+    FunnelStep(event_name="add_to_cart"),
+    FunnelStep(event_name="purchase"),
+]
+purchase_funnel = ga.request_funnel(
+    steps=steps,
+    start=date(2020, 11, 1),
+    end=date(2020, 11, 7),
+    group_by="platform",
+    interval="day",
+)
+print(purchase_funnel.tail())
